34e90de93a9fabe36373909400d983cb310c3f38
adridm97 <adri.d.m1997@gmail.com>
2024-09-12 00:52:26 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrian <adrian@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 20:34:04 by aduenas-          #+#    #+#             */
+/*   Updated: 2024/09/11 22:03:41 by adrian           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+using namespace std;
+
+void strToUpper(string str)
+{    
+    for(int i = 0; str[i]; i++)
+    {
+        str[i] = toupper(str[i]);
+    }
+    cout << str << " ";
+}
+
+int main(int argc, char **argv)
+{
+    string str;
+    if (argc < 2)
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    for (int i = 1; i < argc; i++)
+    {
+        str = argv[i];
+        strToUpper(str);
+    }
+    cout << endl;
+    return (0);
+}
+
